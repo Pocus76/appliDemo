bfdf8bd011a18f055c9da88665f107b797f1f9dc
Pocus <pocus76000@gmail.com>
2019-04-18 11:28:02 +0200

    Initial commit

---
+++ app/src/main/java/fr/pocus/applidemo/LoginActivity.java
@@ -0,0 +1,20 @@
+package fr.pocus.applidemo;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
